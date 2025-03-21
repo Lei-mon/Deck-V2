41e8a473f2ab2ae2684ba5a18b1d785324eae6a8
lei-mon <leilamontemayor37@gmail.com>
2025-03-22 07:27:15 +0800

    moved notif page to a new folder

--- lib/pages/notification/notification.dart
+++ lib/pages/notification/notification.dart
@@ -8,6 +8,7 @@ import '../misc/custom widgets/buttons/custom_buttons.dart';
 import '../misc/custom widgets/buttons/icon_button.dart';
 import '../misc/custom widgets/dialogs/alert_dialog.dart';
 import '../misc/custom widgets/dialogs/confirmation_dialog.dart';
+import '../misc/custom widgets/appbar/auth_bar.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -17,6 +18,10 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return  Scaffold(
+      backgroundColor: DeckColors.backgroundColor,
+      // body: Center
+
+    );
   }
 }
